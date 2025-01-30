57a7820f63db8154251337ef52e5cffdf424fc29
Xavier Figueroa <xfigueroa28@gmail.com>
2025-01-30 17:31:02 -0500

    delete all REST API

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,13 +1,73 @@
+import User from '../models/User.js'
+import { signToken} from '../services/auth.js'
 
+
+interface User {
+    username: string;
+    password: string;
+    email: string;
+}
+
+interface Login {
+    email: string;
+    password: string;
+}
+
+interface Book {
+    bookId: string;
+    title?: string;
+    authors?: string[];
+    description?: string;
+} 
+
+interface Context {
+    user: {_id: string} | null; 
+}
 
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!';
+    me: async (_: any, __: any, context: Context) => {
+      if (!context.user) throw new Error('Not authenticated');
+      return await User.findById(context.user._id);
+    },
+  },
+  Mutation: {
+    createUser: async (_: any, { username, email, password }: User) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(username, email, user._id);
+      return { token, user };
+    },
+    login: async (_: any, { email, password }: Login) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error('No user with this email');
+      }
+      const isPasswordValid = await user.isCorrectPassword(password);
+      if (!isPasswordValid) {
+        throw new Error('Incorrect password');
+      }
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+    saveBook: async (_: any, { bookId, title, authors, description }: Book, context: Context) => {
+      if (!context.user) throw new Error('Not authenticated');
+      return await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: { bookId, title, authors, description } } },
+        { new: true }
+      );
+    },
+    deleteBook: async (_: any, { bookId }: Book, context: Context) => {
+      if (!context.user) throw new Error('Not authenticated');
+      return await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
     },
   },
 };
-  
+
 
 
 export default resolvers;
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,7 +1,33 @@
+
 const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    title: String
+    authors: [String]
+    description: String
+  }
+
+  type Auth {
+    token: String
+    user: User
+  }
 
   type Query {
-    helloWorld: String!
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookId: ID!, title: String, authors: [String], description: String): User
+    deleteBook(bookId: ID!): User
   }
 `;
 
