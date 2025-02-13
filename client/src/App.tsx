61b8ecd6fc95d95a879d286c34336645159e5558
Xavier Figueroa <xfigueroa28@gmail.com>
2025-02-12 21:56:43 -0500

    Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import Auth from './utils/auth';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:3001/graphql', 
+  uri: process.env.REACT_APP_GRAPHQL_URI || '/graphql', 
 });
 
 const authLink = setContext((_, { headers }) => {
